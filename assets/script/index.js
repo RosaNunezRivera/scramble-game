049967a5239dac29a12cb68fadcb46116015efe9
Rosa Nunez <146019823+RosaNunezRivera@users.noreply.github.com>
2023-11-08 17:33:31 -0600

    Initial Project

---
+++ assets/script/index.js
@@ -0,0 +1,169 @@
+'use strict';
+
+// Utility functions
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function selectById(selector, parent = document) {
+    return parent.getElementById(selector);
+}
+
+function selectAll(selector, parent = document) {
+    return [...parent.querySelectorAll(selector)];
+}
+
+function create(element, parent = document) {
+    return parent.createElement(element);
+}
+
+// Function to print  
+function print(...args) {
+    console.log(args.join(', '));
+}
+
+
+//Global variables
+let msg = '';
+let correctWord = '';
+let words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "html",
+        hint: "Language to make a web page"
+    }
+];
+
+//Creating HTML elements 
+let scrambleWord = selectById("scramble-word");
+let hintText = selectById("hint");
+let wordInput = selectById("word");
+
+let buttonPlay = select(".button-play");
+let buttonRestart = select(".button-restart");
+
+
+//Creating HTML audio elements 
+let audioWin = selectById('my-audio-win');
+let audioLost = selectById('my-audio-lost');
+
+//Creating HTML elements feebback message to user
+let message = selectById('feed-back');
+
+
+
+/*--------------------------------------------------------------------------------*/
+/* Function: Star Play Button                                                      */
+/*--------------------------------------------------------------------------------*/
+onEvent('click', buttonPlay, function () {
+    init();
+    showMessage('Enter a Valid word');
+});
+
+init();
+/*--------------------------------------------------------------------------------*/
+/* Function: Initialize the game                                                  */
+/*--------------------------------------------------------------------------------*/
+function init() {
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    scrambleWord.textContent = wordArray.join(" ");
+    hintText.textContent = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();;
+    wordInput.value = "";
+}
+
+/*--------------------------------------------------------------------------------*/
+/* Function: Playing, validating if the word is equeal to the correct word        */
+/*--------------------------------------------------------------------------------*/
+function play() {
+    let wrd = wordInput.value.toLowerCase();
+    if (wrd === correctWord) {
+        audioWin.play();
+        return `Congrats! ${correctWord.toUpperCase()} is the correct word`;
+    } else {
+        audioLost.play();
+        return `Oops! ${wrd} is not the correct word`;
+    }
+}
+
+/*--------------------------------------------------------------------------------*/
+/* Function: Event listener Enter a word  & validate                                               */
+/*--------------------------------------------------------------------------------*/
+wordInput.addEventListener("keydown", function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+
+        let isValid = validate();
+
+        if (isValid) {
+            msg = play();
+            showMessage(msg);
+        }
+    }
+});
+
+/*--------------------------------------------------------------------------------*/
+/* Function: Validation if is a validad word (Not special characters, numbers)    */
+/* or whitespaces                                                                 */
+/*--------------------------------------------------------------------------------*/
+function validate() {
+    let wrd = wordInput.value.toLowerCase();
+
+    const letterPattern = /^[A-Za-z\s]+$/;
+
+    if (wrd.length === 0 || !letterPattern.test(wrd)) {
+        wordInput.value = '';
+        showMessage('Try! Enter a word!')
+        audioLost.play();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/*--------------------------------------------------------------------------------*/
+/* Function: Print message                                                        */
+/*--------------------------------------------------------------------------------*/
+function showMessage(msg) {
+    message.textContent = msg;
+    message.style.display = 'block';
+}
+
+
+// Function to play the audio - word is correct 
+function playSoundWin() {
+    audioWin.play();
+}
+
+// Function to play the audio - word is not correct 
+function playSoundLost() {
+    audioLost.play();
+}
+
+
+
+
